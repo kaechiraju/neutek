dc9f8f1f6771e30112fbf39b801ce2e634480053
aechiraju <kiran.aechiraju@gmail.com>
2022-07-27 00:01:57 -0500

    Adding enum example

---
+++ src/main/java/demo/helper/Day.java
@@ -0,0 +1,6 @@
+package demo.helper;
+
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+    THURSDAY, FRIDAY, SATURDAY
+}
---
+++ src/main/java/demo/helper/JavaConstantsExample.java
@@ -0,0 +1,2 @@
+package demo.helper;public class JavaConstantsExample {
+}
